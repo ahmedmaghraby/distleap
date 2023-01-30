6e5986e57ea26205d2067eec92c81a75dad822cf
ahmed maghraby <38741761+ahmedmaghraby@users.noreply.github.com>
2023-01-30 23:21:07 +0300

    .

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.6e2b0fd9b61c68287a0470f0d12f9d0c.js", "workbox-v4.3.1/workbox-sw.js");
+importScripts("precache-manifest.1d1c2d2fafa2480ca1f22bd3293b6cd6.js", "workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "workbox-v4.3.1"});
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.precaching.suppressWarnings()
